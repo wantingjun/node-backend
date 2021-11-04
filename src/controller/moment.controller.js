7cf7170c6c9fd595c61d7d5be10fa81f3fc5ae8d
wantj1 <wantj1@chinatelecom.cn>
2021-11-04 15:50:32 +0800

    发表动态：插入动态到数据库

--- src/controller/moment.controller.js
+++ src/controller/moment.controller.js
@@ -1,6 +1,14 @@
+const momentService = require("../service/moment.service")
 class MomentController{
     async create(ctx,next){
-        ctx.body = "发表成功"
+        // 1. 获取数据(user_id, content,)
+        const userId = ctx.user.id
+        const content = ctx.request.body.content;
+        console.log(userId,content)
+        //2. 数据插入数据库
+        const result = await momentService.create(userId,content)
+        ctx.body = result
+
 
     }
 }
